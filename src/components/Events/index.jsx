ee0f4761ae2d85064ef0ea43cd6c9e19956fe625
BDiaz-L <bantonio.diaz@hotmail.com>
2026-04-13 19:21:11 -0600

    agregamos json con dummy data

--- src/components/Events/index.jsx
+++ src/components/Events/index.jsx
@@ -1,6 +1,8 @@
 import EventItem from "./Components/EventItem/Index";
-
+import data from "../../data/events.json"
 const Events = () => {
+
+    console.log(data);
     return (
         <div>
             Eventos
