214008bf72e924c31024e82f50be39e55ae34a28
BDiaz-L <bantonio.diaz@hotmail.com>
2026-04-14 19:13:03 -0600

    evitando propagacion de evento click en boton ver mas

--- src/components/Events/Components/EventItem/Index.jsx
+++ src/components/Events/Components/EventItem/Index.jsx
@@ -1,11 +1,16 @@
 const EventItem = ({info, name, image, id, onEventClick}) => {
 
+    const handleSeeMoreClick = (e) => {
+        e.stopPropagation();
+        onEventClick(id);
+    };
+
     return(
-        <div>
+        <div onClick={()=> {console.log('padre clicleado')}}>
             <h4>{name}</h4>
             <p>{info}</p>
             <img src={image} alt={name} width={200} height={200}/>
-            <button onClick={ () => onEventClick(id)}>Ver mas </button>
+            <button onClick={handleSeeMoreClick}>Ver mas </button>
         </div>
     );
 };
